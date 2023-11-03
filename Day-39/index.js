ce25677b8eb168eb1039870d05dcd61442117f46
saiduii <rainingland29@gmail.com>
2023-11-03 19:43:34 +0700

    Nop bai Day-39

--- Day-39/index.js
+++ Day-39/index.js
@@ -1,5 +1,5 @@
 async function fetchData(limit, offset) {
-  const response = await fetch(`http://localhost:3000/posts`);
+  const response = await fetch(`https://glht6v-8080.csb.app/posts`);
   const data = await response.json();
   //   if (data.code === 200) {
   data.forEach((item) => {
