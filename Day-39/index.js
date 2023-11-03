c20419bf7e9b260637a1d8b896166ec435e9f443
saiduii <rainingland29@gmail.com>
2023-11-03 20:10:12 +0700

    Nop bai Day-39

--- Day-39/index.js
+++ Day-39/index.js
@@ -1,7 +1,9 @@
+// fake api
 async function fetchData(limit, offset) {
   const response = await fetch(`https://glht6v-8080.csb.app/posts`);
   const data = await response.json();
-  //   if (data.code === 200) {
+  // response api trả vể
+  // data có dạng array nên forEach lấy ra từng phần tử trong array đấy
   data.forEach((item) => {
     container.innerHTML += ` <div class="blog">
         <div class="title"><a href="#">${item.title}</a></div>
@@ -9,8 +11,12 @@ async function fetchData(limit, offset) {
         <div class="content">${item.content}</div>
       </div>`;
   });
-  //   }
 }
-
+// Chạy 5 data trên 1 trang
 fetchData(5, 1);
 var container = document.querySelector("#container");
+console.log("aa");
+
+document.addEventListener("scroll", (event) => {
+  console.log("aa");
+});
